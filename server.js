1c41e57edd73c0e8ff0da939f89be566a320fb75
Moustafa Elsonbaty <elsonbaty@gmail.com>
2022-10-20 11:41:03 +0400

    usercontroller

--- server.js
+++ server.js
@@ -2,14 +2,19 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('path');
-const { logger } = require('./middleware/logger');
 const errorHandler = require('./middleware/errorHandler');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const corsOptions = require('./config/corsOptions');
+const connectDB = require('./config/dbConn');
+const mongoose = require('mongoose');
+const { logEvents, logger } = require('./middleware/logger');
 const PORT = process.env.PORT || 8000;
 
 console.log('process.env.NODE_ENV', process.env.NODE_ENV);
+
+// Connect to the database
+connectDB();
 
 app.use(logger);
 
@@ -22,6 +27,7 @@ app.use(cookieParser());
 app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.use('/', require('./routes/root'));
+app.use('/users', require('./routes/userRoutes'));
 
 app.all('*', (req, res) => {
   res.status(404);
@@ -35,4 +41,15 @@ app.all('*', (req, res) => {
 });
 
 app.use(errorHandler);
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+mongoose.connection.once('open', () => {
+  console.log('conncted to MongoDB');
+  app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+});
+mongoose.connection.on('error', (err) => {
+  console.log('error connecting to MongoDB', err);
+  logEvents(
+    `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
+    'mongoErrLog.log'
+  );
+});
